0b31732270e214f23a31d649bf0a463af215525d
YohananDiamond <mitonanan12@gmail.com>
2020-03-30 00:01:18 -0300

    Finally made a good opener script
    Made a new opener script. It is based off nuke, which is a script
    available on the nnn GitHub repository. I simplified it and it looks way
    simpler now, in my opinion. I also made another script, openfork, to
    complement the opener. It basically opens the file specified in $1
    asynchronously and dettaches, making it possible for me to, for example,
    open an image in sxiv while in nnn and closing the terminal window with
    nnn, while letting the sxiv window still open.
    
    I've been wanting to do this for quite some time, and I'm happy I
    finally got to finish it.

--- local/share/env.sh
+++ local/share/env.sh
@@ -40,9 +40,7 @@ export WINEPREFIX="$HOME/.cache/wine"
 
 export FZF_DEFAULT_OPTS='--color=16 --height=60% --layout=reverse --border'
 export BAT_THEME="base16"
-# export NNN_OPENER="nuke" # Commented out for now because it seems to
-# be glitching...
-export NNN_OPENER=""
+export NNN_OPENER="openfork"
 export FLAMEDASH_SCR_FOLDER="$HOME/photos/screenshots"
 export BKMK_FILE="$F_WIKI/data/bookmarks"
 
@@ -50,7 +48,6 @@ export KEYTIMEOUT=1
 export TERM="xterm-256color"
 export WM="bspwm"
 
-# Less Config
 export LESS_TERMCAP_mb=$'\e[1;31m'     # begin bold
 export LESS_TERMCAP_md=$'\e[1;35m'     # begin blink
 export LESS_TERMCAP_me=$'\e[0m'        # reset bold/blink
@@ -58,20 +55,18 @@ export LESS_TERMCAP_so=$'\e[33m'       # begin reverse video
 export LESS_TERMCAP_se=$'\e[0m'        # reset reverse video
 export LESS_TERMCAP_us=$'\e[1;32m'     # begin underline
 export LESS_TERMCAP_ue=$'\e[0m'        # reset underline
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
 # Dircolors
 if [ -r ~/.local/share/dircolors ]; then
   eval "$(dircolors -b ~/.local/share/dircolors)"
 fi
 
-# GCC
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
 pathadd "$HOME/.local/bin"
 pathadd "$HOME/.cache/go"
 pathadd "$CARGO_HOME/bin"
-# pathadd "$(ruby -e 'puts Gem.user_dir')/bin" # Seems to be slowing
-# down, so I'll use the ony below and update when needed.
+
+# pathadd "$(ruby -e 'puts Gem.user_dir')/bin" # Seems to be slowing down, so I'll use the ony below and update when needed.
 pathadd "$HOME/.gem/ruby/2.7.0"
 
 # Programs that I've installed in /opt
