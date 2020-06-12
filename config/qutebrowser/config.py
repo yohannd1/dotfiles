e9e2b2ca216ec6c5c9c1b648d935442c0573f01f
YohananDiamond <mitonanan12@gmail.com>
2020-06-12 12:24:59 -0300

    Improved qutebrowser & Script#open config; patched Script#glowrapper

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,47 +1,57 @@
 import subprocess as sp
 import theme
 
+bindings = [
+    ("D", "tab-close"),
+    ("U", "undo"),
+    ("d", "scroll-page 0 0.5"),
+    ("u", "scroll-page 0 -0.5"),
+    ("ç", "set-cmd-text :"),
+]
+
+def main():
+    c.downloads.location.directory = "~/inbox"
+    for (k, cmd) in bindings: config.bind(k, cmd)
+
+    theme.load(c, {
+        "pallete": {
+            "background": xgetres("qutebrowser.background"),
+            "background-alt": xgetres("qutebrowser.background"),
+            "border": xgetres("qutebrowser.border"),
+            "current-line": xgetres("qutebrowser.selection"),
+            "selection": xgetres("qutebrowser.selection"),
+            "foreground": xgetres("qutebrowser.foreground"),
+            "foreground-alt": xgetres("qutebrowser.foreground"),
+            "red": xgetres("qutebrowser.color1"),
+            "green": xgetres("qutebrowser.color2"),
+            "orange": xgetres("qutebrowser.color3"),
+            "yellow": xgetres("qutebrowser.color3"),
+            "pink": xgetres("qutebrowser.color5"),
+            "cyan": xgetres("qutebrowser.color6"),
+            "purple": xgetres("qutebrowser.color4"),
+
+            "background-attention": "#181920",
+            "foreground-attention": "#ffffff",
+            "comment": "#6272a4",
+        },
+        "spacing": {
+            "vertical": 2,
+            "horizontal": 2,
+        },
+    })
+
 def xgetres(*args):
-    proc = sp.Popen(["xgetres", *args], encoding="UTF-8", stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE)
+    proc = sp.Popen(
+        ["xgetres", *args],
+        encoding="UTF-8",
+        stdout=sp.PIPE,
+        stdin=sp.PIPE,
+        stderr=sp.PIPE,
+    )
     proc.stdin.close()
     stdout = proc.stdout.read().strip()
     proc.stdout.close()
     proc.stderr.close()
     return stdout
 
-c.downloads.location.directory = "~/inbox"
-
-for (key, command) in [("D", "tab-close"),
-                       ("U", "undo"),
-                       ("d", "scroll-page 0 0.5"),
-                       ("u", "scroll-page 0 -0.5"),
-                       ("ç", "set-cmd-text :")]:
-    config.bind(key, command)
-
-theme.load(c, {
-    "pallete": {
-        "background": xgetres("qutebrowser.background"),
-        "background-alt": xgetres("qutebrowser.background"),
-        "border": xgetres("qutebrowser.border"),
-        "current-line": xgetres("qutebrowser.selection"),
-        "selection": xgetres("qutebrowser.selection"),
-        "foreground": xgetres("qutebrowser.foreground"),
-        "foreground-alt": xgetres("qutebrowser.foreground"),
-
-        "red": xgetres("qutebrowser.color1"),
-        "green": xgetres("qutebrowser.color2"),
-        "orange": xgetres("qutebrowser.color3"),
-        "yellow": xgetres("qutebrowser.color3"),
-        "pink": xgetres("qutebrowser.color5"),
-        "cyan": xgetres("qutebrowser.color6"),
-        "purple": xgetres("qutebrowser.color4"),
-
-        "background-attention": "#181920",
-        "foreground-attention": "#ffffff",
-        "comment": "#6272a4",
-    },
-    "spacing": {
-        "vertical": 2,
-        "horizontal": 2,
-    },
-})
+main()
