70c238a4f421c7e991664906e0fc30691417367c
YohananDiamond <mitonanan12@gmail.com>
2020-08-03 22:53:08 -0300

    sysm: `Manager` class (and also a small emacs change)

--- config/emacs/modules/main.el
+++ config/emacs/modules/main.el
@@ -17,6 +17,11 @@
                     ice-style-font-family (get-xres "font" ice-style-font-family)
                     ice-style-font-height 100)))
 
+(with-eval-after-load 'evil
+  (defalias #'forward-evil-word #'forward-evil-symbol)
+  ;; make evil-search-word look for symbol rather than word boundaries
+  (setq-default evil-symbol-word-search t))
+
 ;; Backup / autosave files
 ;; TODO: disable backup & autosave altogether if on "foreign" machines.
 (setq version-control t
