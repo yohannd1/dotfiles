6b38722f782082fdff65841cb7786727b57d965f
YohananDiamond <mitonanan12@gmail.com>
2020-08-04 02:10:51 -0300

    Fixed a bug that prevented literally any configuration besides termux config to be symlinked on a termux installation

--- config/dots/sysmrc.py
+++ config/dots/sysmrc.py
@@ -1,7 +1,9 @@
+apps = ()
+
 if m.is_android:
     m.link_conf("termux", "~/.termux")
 else:
-    apps = (
+    apps += (
         # "bspwm",
         # "polybar",
         "sxhkd",
@@ -15,34 +17,37 @@ else:
         # "i3",
         # "rofi",
 
-        "inputrc",
-        "dircolors",
-        "mimeapps.list",
-        "ripgreprc",
-        "dots",
-        "lf",
-        # "broot",
     )
-
-    for app in apps:
-        m.link_conf(app, f"~/.config/{app}")
 
     for i in {2, 3, 4}:
         origin = DOTFILES / f"config/gtk-{i}.0"
         if origin.is_dir():
             m.link_glob(origin, f"~/.config/gtk-{i}.0")
 
-    m.link_conf("profile", "~/.profile")
-    m.link_conf("profile", "~/.zprofile")
-    m.link_conf("zsh/zshrc", "~/.config/zsh/.zshrc")
-    m.link_conf("zsh/zlogout", "~/.config/zsh/.zlogout")
-    m.link_conf("git/gitconfig", "~/.gitconfig")
-    m.link_conf("git/gitignore", "~/.gitignore")
+apps += (
+    "inputrc",
+    "dircolors",
+    "mimeapps.list",
+    "ripgreprc",
+    "dots",
+    "lf",
+    # "broot",
+)
 
-    m.link_glob(DOTFILES / "config/nvim", "~/.config/nvim")
-    m.link_glob(DOTFILES / "config/emacs", "~/.emacs.d")
-    m.link_glob(DOTFILES / "desktop", "~/.local/share/applications")
-    # m.link_glob(DOTFILES / "config/kak", "~/.config/kak")
-    # m.link_glob(DOTFILES / "config/vscode", "~/.config/Code/User")
+for app in apps:
+    m.link_conf(app, f"~/.config/{app}")
+
+m.link_conf("profile", "~/.profile")
+m.link_conf("profile", "~/.zprofile")
+m.link_conf("zsh/zshrc", "~/.config/zsh/.zshrc")
+m.link_conf("zsh/zlogout", "~/.config/zsh/.zlogout")
+m.link_conf("git/gitconfig", "~/.gitconfig")
+m.link_conf("git/gitignore", "~/.gitignore")
+
+m.link_glob(DOTFILES / "config/nvim", "~/.config/nvim")
+m.link_glob(DOTFILES / "config/emacs", "~/.emacs.d")
+m.link_glob(DOTFILES / "desktop", "~/.local/share/applications")
+# m.link_glob(DOTFILES / "config/kak", "~/.config/kak")
+# m.link_glob(DOTFILES / "config/vscode", "~/.config/Code/User")
 
 os.system(DOTFILES / "scripts/gen-config")
