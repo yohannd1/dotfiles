dd5ffe62294749184c243b341ada07b8ec3c9189
YohananDiamond <mitonanan12@gmail.com>
2020-08-03 23:30:57 -0300

    Crossing fingers for this to work

--- config/dots/sysmrc.py
+++ config/dots/sysmrc.py
@@ -1,1 +1,43 @@
-print(10)
+if m.is_android:
+    m.link_conf("termux", "~/.termux")
+else:
+    apps = (
+        "bspwm",
+        "polybar",
+        "sxhkd",
+        "alacritty",
+        "picom",
+        "xorg",
+        "awesome",
+        "zathura",
+        "taskwarrior",
+        "sxiv",
+        # "i3",
+        # "rofi",
+
+        "inputrc",
+        "dircolors",
+        "mimeapps.list",
+        "ripgreprc",
+        "dots",
+        "lf",
+        "broot",
+    )
+
+    for app in apps:
+        m.link_conf(app, f"~/.config/{app}")
+
+    for i in {2, 3}:
+        m.link_glob(DOTFILES / f"config/gtk-{i}.0", f"~/.config/gtk-{i}.0")
+
+    m.link_conf("profile", "~/.profile")
+    m.link_conf("profile", "~/.zprofile")
+    m.link_conf("zsh/zshrc", "~/.config/zsh/.zshrc")
+    m.link_conf("zsh/zlogout", "~/.config/zsh/.zlogout")
+    m.link_conf("git/gitconfig", "~/.gitconfig")
+    m.link_conf("git/gitignore", "~/.gitignore")
+
+    m.link_glob(DOTFILES / "config/nvim", "~/.config/nvim")
+    m.link_glob(DOTFILES / "config/emacs", "~/.emacs.d")
+    # m.link_glob(DOTFILES / "config/kak", "~/.config/kak")
+    # m.link_glob(DOTFILES / "config/vscode", "~/.config/Code/User")
