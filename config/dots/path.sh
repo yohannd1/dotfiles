666699a23f30549128a4dc2591ef682ec847a9fa
YohananDiamond <mitonanan12@gmail.com>
2021-07-18 16:37:57 -0300

    feat: LUA_PATH support for Lua 5.1 / LuaJIT

--- config/dots/path.sh
+++ config/dots/path.sh
@@ -34,6 +34,7 @@ _luaPkgsAt() {
   printf "%s/?.lua;%s/?/init.lua" "$1" "$1"
 }
 
+# Paths for lua 5.2 and on
 for luaVer in 2 3 4; do
   shareDir="$LUAROCKS_HOME/share/lua/5.${luaVer}"
   libDir="$LUAROCKS_HOME/lib/lua/5.${luaVer}"
@@ -42,3 +43,8 @@ for luaVer in 2 3 4; do
   eval "export LUA_PATH_5_${luaVer}='$(_luaPkgsAt "$shareDir");$(_luaPkgsAt "$libDir");$(_luaPkgsAt "$dotfLibDir");;'"
   eval "export LUA_CPATH_5_${luaVer}='$libDir/loadall.so;$libDir/?.so;;'"
 done
+
+# Paths for lua 5.1
+libDir="$LUAROCKS_HOME/lib/lua/5.1"
+eval "export LUA_PATH='$(_luaPkgsAt "$LUAROCKS_HOME/share/lua/5.1");$(_luaPkgsAt "$libDir");$(_luaPkgsAt "$DOTFILES/lib");;'"
+eval "export LUA_CPATH='$libDir/loadall.so;$libDir/?.so;;'"
