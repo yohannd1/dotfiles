416c50d54f7084ebf1f95a84196850341823e470
yohannd1 <mitonanan12@gmail.com>
2025-12-31 14:26:21 -0300

    had to come back to qtile for a few minutes

--- config/qtile/settings/keys.py
+++ config/qtile/settings/keys.py
@@ -40,66 +40,43 @@ def load_general_keymap(cfg) -> list[Key]:
 def make_keyboard_map(cfg) -> list[Key]:
     keys = []
 
-    keys += [
+    keys += [ # fmt: off
         Key([mod], "j", *next_window_actions, desc="Go to next window"),
         Key([mod], "k", *prev_window_actions, desc="Go to previous window"),
         Key([mod], "h", lazy.layout.shrink_main(), desc="Shrink main window"),
         Key([mod], "l", lazy.layout.grow_main(), desc="Expand main window"),
-        Key([mod, shift], "h", lazy.layout.shrink(), desc="Shrink current window"),
-        Key([mod, shift], "l", lazy.layout.grow(), desc="Expand current window"),
-        Key(
-            [mod, shift], "space", lazy.window.toggle_floating(), desc="toggle floating"
-        ),
-        Key(
-            [mod],
-            "f",
-            lazy.layout.toggle_focus_floating(),
-        ),
+        Key([mod, alt], "h", lazy.layout.shrink(), desc="Shrink current window"),
+        Key([mod, alt], "l", lazy.layout.grow(), desc="Expand current window"),
+        Key([mod, shift], "space", lazy.window.toggle_floating(), desc="toggle floating"),
+        Key([mod], "f", lazy.layout.toggle_focus_floating(),),
         Key([mod], "v", lazy.window.bring_to_front()),
-        Key(
-            [mod, shift],
-            "j",
-            lazy.layout.shuffle_down(),
-            desc="Move window down/forward",
-        ),
-        Key(
-            [mod, shift], "k", lazy.layout.shuffle_up(), desc="Move window up/backward"
-        ),
+        Key([mod, shift], "j", lazy.layout.shuffle_down(), desc="Move window down/forward",),
+        Key([mod, shift], "k", lazy.layout.shuffle_up(), desc="Move window up/backward"),
         Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-        Key([mod], "Tab", lazy.next_layout(), desc="Go to next layout"),
-        Key([mod, shift], "Tab", lazy.prev_layout(), desc="Go to previous layout"),
+        Key([mod], "ç", lazy.next_layout(), desc="Go to next layout"),
+        Key([mod, shift], "ç", lazy.prev_layout(), desc="Go to previous layout"),
         Key([mod], "q", lazy.window.kill(), desc="Close focused window"),
         Key([mod, ctrl, alt], "r", lazy.restart(), desc="Restart Qtile"),
         Key([mod, ctrl, alt], "e", lazy.shutdown(), desc="Leave Qtile"),
-        Key(
-            [mod, shift],
-            "f",
-            lazy.window.toggle_fullscreen(),
-            desc="Toggle fullscreen window",
-        ),
+        Key([mod, shift], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen window",),
         Key([mod], "u", manual_updates, desc="Manual update"),
         Key([mod, alt], "space", switch_to_window, desc="Switch to window"),
         # Key([mod], "Return", lazy.spawn(cfg.terminal),
         #     desc="Spawn terminal"),
-    ]
+    ] # fmt: on
 
     for i in cfg.groups:
+        # fmt: off
         keys += [
             # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
+            Key([mod], i.name, lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name)),
+
             # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, shift],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=False),
-                desc="Move focused window to group {}".format(i.name),
-            ),
+            Key([mod, shift], i.name, lazy.window.togroup(i.name, switch_group=False),
+                desc="Move focused window to group {}".format(i.name)),
         ]
+        # fmt: on
 
     for vt in range(1, 8):
         keys.append(
